73bf192e7a501adf188a2c3557c02813e83b6877
vansteki <vansteki.tw@gmail.com>
2015-10-17 17:27:58 +0800

    IronPeter 啟動! :cat2:

---
+++ util/ironPeter/ironPeter.js
@@ -0,0 +1,12 @@
+https = require("https")
+
+var url = 'https://mr-overlord.herokuapp.com'
+https.get(url, function(res, err) {
+	console.log("GET " + url)
+	res.on('data', function(chunk) {
+		if (chunk.toString() == "Cannot GET /\n")
+			console.log("GET DONE", new Date)
+		else
+			console.log("WARNING: WIRED STATE")
+	})
+})
